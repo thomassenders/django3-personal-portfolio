280498f8f60940f3db13a774326a2089cfe217db
Thomas Senders <thomas.senders@nl.ey.com>
2020-08-05 15:15:08 +0200

    Fifth Commit

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 from datetime import date
 
-today = date.today()
+today = django.utils.timezone.now
 
 class Blog(models.Model):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=250)
-    # image = models.ImageField(upload_to='blog/images/')
     date = models.DateField(default=today)
 
     def __str__(self):
